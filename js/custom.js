926488570ba94e54855e483cc72214a89efc6b26
Judah Turner <judahturner@gmail.com>
2022-09-01 12:08:36 +0900

    updates to JS and CSS

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,9 @@
 // };
 
 // Version 1 - Michi coded this version
+let status = [false, false, false, false, false];
+let temporaryName = document.querySelectorAll();
+
 function focusInactive() {
   let inactive = document.getElementById("inactive-favorite").style.zIndex;
   let active = document.getElementById("active-favorite").style.zIndex;
@@ -33,76 +36,72 @@ function focusActive() {
   }
 }
 
-
 // Version 2 - Judah coded this version, using classes
 // Using queryselector to turn on and off classes, displaying img as block or none.
-const greyPin = document.querySelector('.closed');
-const yellowPin = document.querySelector('.open');
+const greyPin = document.querySelector(".closed");
+const yellowPin = document.querySelector(".open");
 
 //Add event listener
-greyPin.addEventListener('click', () => {
-  if(yellowPin.classList.contains('open')) { //using classes
-    yellowPin.classList.add('activeblock');
-    greyPin.classList.remove('activeblock');
+greyPin.addEventListener("click", () => {
+  if (yellowPin.classList.contains("open")) {
+    //using classes
+    yellowPin.classList.add("activeblock");
+    greyPin.classList.remove("activeblock");
   }
 });
 
-yellowPin.addEventListener('click', () => {
-  if(greyPin.classList.contains('closed')) { //using classes
-    greyPin.classList.add('activeblock');
-    yellowPin.classList.remove('activeblock');
+yellowPin.addEventListener("click", () => {
+  if (greyPin.classList.contains("closed")) {
+    //using classes
+    greyPin.classList.add("activeblock");
+    yellowPin.classList.remove("activeblock");
   }
 });
-
-
 
 // Version 3 - Judah coded this, using variables
 
 // Using queryselector to turn on and off classes, displaying img as block or none.
-const greyPin = document.querySelector('.closed');
-const yellowPin = document.querySelector('.open');
+const greyPin = document.querySelector(".closed");
+const yellowPin = document.querySelector(".open");
 
 //Add event listener
-greyPin.addEventListener('click', () => {
-  if(greyPin) { //using just variable
-    yellowPin.classList.add('activeblock');
-    greyPin.classList.remove('activeblock');
+greyPin.addEventListener("click", () => {
+  if (greyPin) {
+    //using just variable
+    yellowPin.classList.add("activeblock");
+    greyPin.classList.remove("activeblock");
   }
 });
 
-yellowPin.addEventListener('click', () => {
-  if(yellowPin) { //using just variable
-    greyPin.classList.add('activeblock');
-    yellowPin.classList.remove('activeblock');
+yellowPin.addEventListener("click", () => {
+  if (yellowPin) {
+    //using just variable
+    greyPin.classList.add("activeblock");
+    yellowPin.classList.remove("activeblock");
   }
 });
-
-
-
-
-
 
 // Version 4 - Judah coded this, using boolean, but I don't know how to finish it.
 // Boolean function
-let favorite = document.querySelector('.favorite');
+let favorite = document.querySelector(".favorite");
 let greyPin = false;
 
-favorite.addEventListener("click", function() {
-  if(greyPin){
-    document.querySelector('.grey').style.display = "block";
-    document.querySelector('.yellow').style.display = "none";
+favorite.addEventListener("click", function () {
+  if (greyPin) {
+    document.querySelector(".grey").style.display = "block";
+    document.querySelector(".yellow").style.display = "none";
   } else {
-    document.querySelector('.yellow').style.display = "block";
-    document.querySelector('.grey').style.display = "none";
+    document.querySelector(".yellow").style.display = "block";
+    document.querySelector(".grey").style.display = "none";
   }
 });
 
-favorite.addEventListener("click", function() {
-  if(!greyPin){
-    document.querySelector('.yellow').style.display = "block";
-    document.querySelector('.grey').style.display = "none";
+favorite.addEventListener("click", function () {
+  if (!greyPin) {
+    document.querySelector(".yellow").style.display = "block";
+    document.querySelector(".grey").style.display = "none";
   } else {
-    document.querySelector('.grey').style.display = "block";
-    document.querySelector('.yellow').style.display = "none";
+    document.querySelector(".grey").style.display = "block";
+    document.querySelector(".yellow").style.display = "none";
   }
 });
