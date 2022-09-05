377951588dd2ec189dee11d238e762598fda64da
Judah Turner <judahturner@gmail.com>
2022-09-05 11:52:44 +0900

    New JS file, finished click event function

---
+++ js/cars.js
@@ -0,0 +1,26 @@
+// btn will be favorite
+// A will be grey
+// B will be yellow
+let elements = document.querySelectorAll(".favorite");
+let flags = [];
+const child1 = ".grey";
+const child2 = ".yellow";
+
+// initialize flags
+for (let i = 0; i < elements.length; i++) {
+  flags[i] = false;
+}
+
+// Add event listener all elements that have 'btn' class
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function () {
+    flags[i] = !flags[i];
+    if (flags[i]) {
+      elements[i].querySelector(child1).style.display = "block";
+      elements[i].querySelector(child2).style.display = "none";
+    } else {
+      elements[i].querySelector(child1).style.display = "none";
+      elements[i].querySelector(child2).style.display = "block";
+    }
+  });
+}
